c7a848ecc15bc22148e0f6f2be4ec7a612bf7309
l0u0h0 <l0u0h0@gmail.com>
2023-10-11 23:44:25 +0900

    BJ_13460 :construction:

--- BaekJoon/BJ13460.java
+++ BaekJoon/BJ13460.java
@@ -5,24 +5,64 @@ import java.io.*;
 
 public class BJ13460 {
     static int N, M;
+    static int[] goal;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    // 기울어진 방향으로 구슬 움직이는 함수
+    private static int[] move(int[] dxdy, int[] ball, String[][] map) {
+        while (true) {
+            if (map[])
+        }
+    }
+
+    private static int dfs(int[] red, int[] blue, int depth, String[][] map) {
+        // 리턴 분기점
+
+        // 사방 탐색 & 재귀 호출
+        // 빨간 구슬 사방 탐색
+        for (int i = 0; i < 4; i++) {
+            int redR = dx[i];
+            int redC = dy[i];
+
+            // 파란 구슬 사방 탐색
+            for (int j = 0; j < 4; j++) {
+                int blueR = dx[j];
+                int blueC = dy[j];
+            }
+        }
+    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = null;
+        String[] line;
         String str = br.readLine();
         N = Integer.parseInt(str.split(" ")[0]);
         M = Integer.parseInt(str.split(" ")[1]);
         String[][] map = new String[N][M];
-        System.out.println(N + " " + M);
+        int[] red = new int[2];
+        int[] blue = new int[2];
+        goal = new int[2];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            line = br.readLine().split("");
             for (int j = 0; j < M; j++) {
-                map[i][j] = st.nextToken();
-                System.out.print(map[i][j]);
+                map[i][j] = line[j];
+                if (line[j].equals("B")) {
+                    blue[0] = i;
+                    blue[1] = j;
+                } else if (line[j].equals("R")) {
+                    red[0] = i;
+                    red[1] = j;
+                } else if (line[j].equals("O")) {
+                    goal[0] = i;
+                    goal[1] = j;
+                }
             }
-            System.out.println();
         }
+
+
 
     }
 }
